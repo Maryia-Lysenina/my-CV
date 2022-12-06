e77cbb6795a82ae472681ab10de82cb5409108cf
Maria <lysenina.maria@gmail.com>
2022-12-06 21:57:40 +0300

    burger-menu added

---
+++ script.js
@@ -0,0 +1,16 @@
+let burger = document.querySelector('.header__burger');
+let nav = document.querySelector('.main-nav-tablet');
+burger.addEventListener('click', function(){
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+    document.body.classList.toggle('active');
+    grey.classList.toggle('active');
+});
+    function closeMenu(){
+        burger.classList.remove('active');
+        nav.classList.remove('active');
+        grey.classList.remove('active');
+        document.body.classList.remove('active');
+        };
+let navLinks = document.querySelectorAll('.main-nav-tablet .nav__link');
+navLinks.forEach((el) => el.addEventListener('click', closeMenu));
